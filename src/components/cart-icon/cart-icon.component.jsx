cf4898a969b17bb155613d6332aa84bfa01a418a
influxion <nicholsjordon721@gmail.com>
2022-09-12 10:42:48 -0500

    Fixed cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,10 +9,7 @@ import './cart-icon.styles.scss';
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
 
-  const toggleIsCartOpen = () => {
-    console.log(isCartOpen);
-    setIsCartOpen(!isCartOpen);
-  };
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
